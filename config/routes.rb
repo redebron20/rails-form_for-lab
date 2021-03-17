fd3218dde572264ea534945b7cb37ecb671db884
redebron20 <redebron20@gmail.com>
2021-03-17 19:36:12 -0400

    commit tree 6f51695f62c7844a187fec8dc12bfbf7695f8231

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:new, :create, :edit, :update]
+  get '/students/:id', to: 'students#show'
+  
+  resources :school_classes, only: [:new, :create, :edit, :update]
+  get '/school_classes/:id', to: 'school_classes#show'
 end
